2e3fc79bdc0840ca0d160cc816be60abf57df7d7
nidhin <nidhinkichoos77366@gmail.com>
2024-11-13 18:06:29 +0530

    java file

---
+++ VowelOrNot.java
@@ -0,0 +1,25 @@
+ class VowelOrNot {
+
+	public static void main(String[] args) {
+		Scanner scanner = new(System.in);
+		System.out.print("Enter an alphabet: ");
+        char ch = scanner.next().charAt(0);
+       
+        switch (Character.toLowerCase(ch)) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            System.out.println(ch + " is a vowel.");
+            break;
+        default:
+            System.out.println(ch + " is not a vowel.");
+            break;
+    }
+
+    scanner.close();
+}
+
+
+	}
